b06bdadf3301374459a8fa603ef9c63da6d48290
3bsamad0x90 <mohamedabdelsamad122@gmail.com>
2023-08-12 19:54:26 +0300

    create event and listener and register them

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Crm\Customer\Events\CustomerCreation;
+use Crm\Customer\Listeners\NotifySalesOnCustomerCreation;
+use Crm\Customer\Listeners\SendWelcomeEmail;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -17,6 +20,11 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+
+        CustomerCreation::class => [
+            NotifySalesOnCustomerCreation::class,
+            SendWelcomeEmail::class,
         ],
     ];
 
