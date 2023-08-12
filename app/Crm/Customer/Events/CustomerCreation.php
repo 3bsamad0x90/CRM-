425705f1e2ca3b2890a8f55d53d3bd68c40670e4
3bsamad0x90 <mohamedabdelsamad122@gmail.com>
2023-08-12 20:06:41 +0300

    create setter and getter in CustomerCreation event

--- app/Crm/Customer/Events/CustomerCreation.php
+++ app/Crm/Customer/Events/CustomerCreation.php
@@ -2,6 +2,7 @@
 
 namespace Crm\Customer\Events;
 
+use Crm\Customer\Models\Customer;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -14,14 +15,27 @@ class CustomerCreation
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    private Customer $customer;
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Customer $customer)
     {
-        //
+        $this->setCustomer($customer);
+    }
+
+    // create a getter for the customer property
+    public function getCustomer(): Customer
+    {
+        return $this->customer;
+    }
+
+    // create a setter for the customer property
+    public function setCustomer(Customer $customer): void
+    {
+        $this->customer = $customer;
     }
 
     /**
